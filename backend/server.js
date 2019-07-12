de90d26cbe090abc5bc452189f3e2d03caf54224
Ben Pinchas <benphs5@gmail.com>
2019-07-12 14:13:28 +0300

    basic server initilized

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+console.log('server run!');
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+const app = express()
+const http = require('http').createServer(app);
+
+
+app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+
+if (process.env.NODE_ENV !== 'production') {
+    const corsOptions = {
+        origin: 'http://127.0.0.1:8080',
+        credentials: true
+    };
+    app.use(cors(corsOptions));
+}
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')));
+}
+
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+    console.log('Server is running on port: ' + port)
+});
