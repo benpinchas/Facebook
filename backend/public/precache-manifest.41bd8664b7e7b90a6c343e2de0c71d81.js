f9cb599067e63b909e562c2478d363470dde6f3a
Ben Pinchas <benphs5@gmail.com>
2019-07-15 11:16:25 +0300

    first build

---
+++ backend/public/precache-manifest.41bd8664b7e7b90a6c343e2de0c71d81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "94f10bf84815dcc0912a",
+    "url": "/js/chunk-vendors.d6690604.js"
+  },
+  {
+    "revision": "3e1ef9d761e590ae5296",
+    "url": "/js/app.b0602d3d.js"
+  },
+  {
+    "revision": "c44ed6079e22df95cf50a8296beee554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7abf9b4efa2e8124382c276854a4bb8e",
+    "url": "/img/like.7abf9b4e.png"
+  },
+  {
+    "revision": "9a07df4e3b02be67d20bcfe1f67bd660",
+    "url": "/css/main.css"
+  },
+  {
+    "revision": "3e1ef9d761e590ae5296",
+    "url": "/css/app.d4e7a9ee.css"
+  }
+];
